cbb79707698e0d6c097530cfcdde4fae084798fb
AlexKharenko <alexey16052002@gmail.com>
2023-12-02 02:13:05 +0200

    mock data

---
+++ src/data/cities.ts
@@ -0,0 +1,73 @@
+import type { City } from '@/types/city.type';
+export const cities: City[] = [
+    { id: 1, cityName: 'Tokyo', countryShort: 'JP', latitude: 35.6895, longitude: 139.6917 },
+    { id: 2, cityName: 'New York', countryShort: 'US', latitude: 40.7128, longitude: -74.006 },
+    { id: 3, cityName: 'London', countryShort: 'GB', latitude: 51.5074, longitude: -0.1278 },
+    { id: 4, cityName: 'Paris', countryShort: 'FR', latitude: 48.8566, longitude: 2.3522 },
+    { id: 5, cityName: 'Beijing', countryShort: 'CN', latitude: 39.9042, longitude: 116.4074 },
+    { id: 6, cityName: 'Kyiv', countryShort: 'UA', latitude: 55.7558, longitude: 37.6176 },
+    {
+        id: 7,
+        cityName: 'Rio de Janeiro',
+        countryShort: 'BR',
+        latitude: -22.9068,
+        longitude: -43.1729
+    },
+    { id: 8, cityName: 'Sydney', countryShort: 'AU', latitude: -33.8688, longitude: 151.2093 },
+    { id: 9, cityName: 'Cairo', countryShort: 'EG', latitude: 30.033, longitude: 31.2336 },
+    { id: 10, cityName: 'Istanbul', countryShort: 'TR', latitude: 41.0082, longitude: 28.9784 },
+    { id: 11, cityName: 'Mumbai', countryShort: 'IN', latitude: 19.076, longitude: 72.8777 },
+    {
+        id: 12,
+        cityName: 'Mexico City',
+        countryShort: 'MX',
+        latitude: 19.4326,
+        longitude: -99.1332
+    },
+    { id: 13, cityName: 'Berlin', countryShort: 'DE', latitude: 52.52, longitude: 13.405 },
+    { id: 14, cityName: 'Toronto', countryShort: 'CA', latitude: 43.6532, longitude: -79.3832 },
+    { id: 15, cityName: 'Seoul', countryShort: 'KR', latitude: 37.5665, longitude: 126.978 },
+    { id: 16, cityName: 'Bangkok', countryShort: 'TH', latitude: 13.7563, longitude: 100.5018 },
+    { id: 17, cityName: 'Rome', countryShort: 'IT', latitude: 41.9028, longitude: 12.4964 },
+    { id: 18, cityName: 'Cape Town', countryShort: 'ZA', latitude: -33.9249, longitude: 18.4241 },
+    { id: 19, cityName: 'Dubai', countryShort: 'AE', latitude: 25.2769, longitude: 55.2962 },
+    {
+        id: 20,
+        cityName: 'Los Angeles',
+        countryShort: 'US',
+        latitude: 34.0522,
+        longitude: -118.2437
+    },
+    { id: 21, cityName: 'Singapore', countryShort: 'SG', latitude: 1.3521, longitude: 103.8198 },
+    { id: 22, cityName: 'Hong Kong', countryShort: 'HK', latitude: 22.3193, longitude: 114.1694 },
+    { id: 23, cityName: 'Berlin', countryShort: 'DE', latitude: 52.52, longitude: 13.405 },
+    { id: 24, cityName: 'Stockholm', countryShort: 'SE', latitude: 59.3293, longitude: 18.0686 },
+    { id: 25, cityName: 'Tokyo', countryShort: 'JP', latitude: 35.6895, longitude: 139.6917 },
+    { id: 26, cityName: 'Shanghai', countryShort: 'CN', latitude: 31.2304, longitude: 121.4737 },
+    {
+        id: 27,
+        cityName: 'San Francisco',
+        countryShort: 'US',
+        latitude: 37.7749,
+        longitude: -122.4194
+    },
+    { id: 28, cityName: 'Amsterdam', countryShort: 'NL', latitude: 52.3676, longitude: 4.9041 },
+    {
+        id: 29,
+        cityName: 'Johannesburg',
+        countryShort: 'ZA',
+        latitude: -26.2041,
+        longitude: 28.0473
+    },
+    { id: 30, cityName: 'Toronto', countryShort: 'CA', latitude: 43.6532, longitude: -79.3832 },
+    { id: 31, cityName: 'Munich', countryShort: 'DE', latitude: 48.8566, longitude: 2.3522 },
+    { id: 32, cityName: 'Istanbul', countryShort: 'TR', latitude: 41.0082, longitude: 28.9784 },
+    { id: 33, cityName: 'Sydney', countryShort: 'AU', latitude: -33.8688, longitude: 151.2093 },
+    { id: 34, cityName: 'Barcelona', countryShort: 'ES', latitude: 41.3851, longitude: 2.1734 },
+    { id: 35, cityName: 'Bangalore', countryShort: 'IN', latitude: 12.9716, longitude: 77.5946 },
+    { id: 36, cityName: 'Oslo', countryShort: 'NO', latitude: 59.9139, longitude: 10.7522 },
+    { id: 37, cityName: 'Dublin', countryShort: 'IE', latitude: 53.3498, longitude: -6.2603 },
+    { id: 38, cityName: 'Seoul', countryShort: 'KR', latitude: 37.5665, longitude: 126.978 },
+    { id: 39, cityName: 'Cairo', countryShort: 'EG', latitude: 30.033, longitude: 31.2336 },
+    { id: 40, cityName: 'Lviv', countryShort: 'UA', latitude: 49.84, longitude: 24.03 }
+];
